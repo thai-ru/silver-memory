9b9f05d6a45b12dca5f7c87ded39009ac8464877
thai-ru <robinthairu34@gmail.com>
2023-12-02 18:45:47 +0300

    ok, idiom, Delete keyword

--- maps.go
+++ maps.go
@@ -15,8 +15,34 @@ func main() {
 
 	//	 Non-empty map literal
 	teams := map[string][]string{
-		"Lions": []string{"John", "Joseph"},
-		"Bulls": []string{"lona", "Sarah"},
+		"Lions": {"John", "Joseph"},
+		"Bulls": {"lona", "Sarah"},
 	}
 	fmt.Println(teams)
+
+	//	empty map literal
+	teamWins["Lions"] = 12
+	teamWins["Bulls"]++
+	fmt.Println(teamWins["Lions"])
+	fmt.Println(teamWins["Bulls"])
+
+	//	ok idiom
+
+	m := map[string]int{
+		"hello": 5,
+		"world": 0,
+	}
+	v, ok := m["hello"]
+	fmt.Println(v, ok)
+
+	v, ok = m["bye"]
+	fmt.Println(v, ok)
+
+	v, ok = m["world"]
+	fmt.Println(v, ok)
+
+	//	Deleting from a map
+	delete(m, "hello")
+	fmt.Println(m)
+
 }
