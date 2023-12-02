99074f79cac8735b542d6e5d7d082829d9e013c5
thai-ru <robinthairu34@gmail.com>
2023-12-02 20:28:26 +0300

    dot notation in structs

--- structs.go
+++ structs.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+	pet  string
+}
+
+func main() {
+	var fred Person
+	fmt.Println(fred)
+
+	julie := Person{
+		"Julie",
+		40,
+		"Cat",
+	}
+
+	fmt.Println(julie)
+
+	beth := Person{
+		name: "Beth",
+		age:  23,
+	}
+
+	fmt.Println(beth)
+
+	//Dotted notation for reading and writing into a struct field
+	fmt.Println(beth.name)
+}
