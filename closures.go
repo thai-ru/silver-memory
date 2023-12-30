6d9a45df4412f1c6272700312f274563a4ce0bb3
thai-ru <robinthairu34@gmail.com>
2023-12-30 10:11:10 +0300

    closures

---
+++ closures.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// These are functions inside a functions that can access and modify variable in the outer function.
+func main() {
+	type Animal struct {
+		FirstName string
+		LastName  string
+		Age       int
+	}
+
+	animals := []Animal{
+		{"John", "Loden", 19},
+		{"Jane", "Kamau", 33},
+		{"Peter", "Parker", 25},
+	}
+
+	fmt.Println(animals)
+
+	sort.Slice(animals, func(i, j int) bool {
+		return animals[i].LastName < animals[j].LastName
+	})
+	fmt.Println(animals)
+
+	sort.Slice(animals, func(i, j int) bool {
+		return animals[i].Age < animals[j].Age
+	})
+	fmt.Println(animals)
+}
