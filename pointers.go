bc41f31e58bbf24db2d9925b5b5d2aa1502f8364
thai-ru <robinthairu34@gmail.com>
2024-01-13 07:07:53 +0300

    pointers

--- pointers.go
+++ pointers.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	l := 10
+	pointerToL := &l
+	fmt.Println(l)
+	fmt.Println(pointerToL)  // this prints the memory address of l
+	fmt.Println(*pointerToL) // dereferencing the memory address of l
+
+	h := 5 + *pointerToL
+	fmt.Println(h)
+}
